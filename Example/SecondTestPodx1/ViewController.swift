e0049fdcabe5ea8f792bcfca1c372af124ec1d34
Aniket Deshmukh <aniket.deshmukh@capgemini.com>
2018-08-24 18:32:05 +0530

    Updated Example Project

--- Example/SecondTestPodx1/ViewController.swift
+++ Example/SecondTestPodx1/ViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import SecondTestPodx1
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        SecondTestPod().show()
     }
 
     override func didReceiveMemoryWarning() {
